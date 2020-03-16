b0255396b6fea22db2da4cb4ff88403d25fe9875
Olivier De Meulder <odemeulder@gmail.com>
2020-03-15 22:44:05 -0400

    store.py

--- store.py
+++ store.py
@@ -3,7 +3,7 @@ import boto3
 from datetime import date
 
 STORE_PATH = "./store/store.json"
-BUCKET = "odm_linecount"
+BUCKET = "odm-linecount"
 BUCKET_KEY = "store.json"
 ENCODING = "UTF-8"
 
@@ -15,7 +15,8 @@ def load(store_path = ""):
   # with open(store_path) as fp: 
   #   return json.load(fp) 
   content_object = s3client.Object(BUCKET, BUCKET_KEY)
-  file_content = content_object.get()['Body'].read().decode(ENCODING)
+  # file_content = content_object.get()['Body'].read().decode(ENCODING)
+  file_content = content_object.get()['Body']
   return json.load(file_content)
 
 def save(store, store_path = ""):
@@ -25,7 +26,8 @@ def save(store, store_path = ""):
   #   json.dump(store, fp)
   content_object = s3client.Object(BUCKET, BUCKET_KEY)
   content_object.put(
-    Body=(bytes(json.dump(store).encode(ENCODING)))
+    # Body=(bytes(json.dump(store).encode(ENCODING)))
+    Body=json.dump(store)
   )
 
 
